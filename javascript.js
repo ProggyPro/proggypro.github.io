0d2215220687f264eb40c2e1dd22619123d01f1f
ProggyPro <114471956+ProggyPro@users.noreply.github.com>
2022-09-27 10:00:00 +0200

    V0.0.2

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
 window.onload = function() {
-    document.getElementById("hello").innerHTML = "Page Loaded Sucessfully";
+    // Initialize the firebase project
+    var config = {
+        apiKey: "AIzaSyBjjbdAXbDII5nAlT80jAqfNvJN6uqfCSk",
+        authDomain: "firebasics-a513a.firebaseapp.com",
+        databaseURL: "https://firebasics-a513a-default-rtdb.firebaseio.com",
+        projectId: "firebasics-a513a",
+        storageBucket: "firebasics-a513a.appspot.com",
+        messagingSenderId: "540478333952",
+        appId: "1:540478333952:web:125cc9106962b88be364e5",
+        measurementId: "G-GMVR1K1GYX"
+    };
+    firebase.initializeApp(config);
+
+    // Initialize the firestore database
+    var db = firebase.firestore();
+
+    // Get the first item from the collection "test"
+    db.collection("test").doc("EuquIyNp3f35unkoZuu9").onSnapshot(function(doc) {
+        document.getElementById("hello").innerHTML = doc.data().text;
+    });
 }
